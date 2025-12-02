8bc8e452e98bdb09af1ad15c2a0a0f2d2ef12683
Nazarii Lozynskyi <lozynsky30@gmail.com>
2025-12-02 20:43:22 +0100

    Add project files.

---
+++ Projekt_Architektura/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Projekt_Architektura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNum = new System.Windows.Forms.TextBox();
+            this.convertedFirstNum = new System.Windows.Forms.TextBox();
+            this.secondNum = new System.Windows.Forms.TextBox();
+            this.convertedSecondNum = new System.Windows.Forms.TextBox();
+            this.resultBin = new System.Windows.Forms.TextBox();
+            this.resultDeci = new System.Windows.Forms.TextBox();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNum
+            // 
+            this.firstNum.Location = new System.Drawing.Point(38, 106);
+            this.firstNum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.firstNum.MaxLength = 32;
+            this.firstNum.Name = "firstNum";
+            this.firstNum.Size = new System.Drawing.Size(232, 20);
+            this.firstNum.TabIndex = 0;
+            this.firstNum.TextChanged += new System.EventHandler(this.firstNum_TextChanged);
+            this.firstNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.firstNum_KeyPress);
+            // 
+            // convertedFirstNum
+            // 
+            this.convertedFirstNum.Enabled = false;
+            this.convertedFirstNum.Location = new System.Drawing.Point(38, 165);
+            this.convertedFirstNum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.convertedFirstNum.Name = "convertedFirstNum";
+            this.convertedFirstNum.Size = new System.Drawing.Size(232, 20);
+            this.convertedFirstNum.TabIndex = 1;
+            // 
+            // secondNum
+            // 
+            this.secondNum.Location = new System.Drawing.Point(290, 106);
+            this.secondNum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.secondNum.MaxLength = 32;
+            this.secondNum.Name = "secondNum";
+            this.secondNum.Size = new System.Drawing.Size(254, 20);
+            this.secondNum.TabIndex = 2;
+            this.secondNum.TextChanged += new System.EventHandler(this.secondNum_TextChanged);
+            this.secondNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.secondNum_KeyPress);
+            // 
+            // convertedSecondNum
+            // 
+            this.convertedSecondNum.Enabled = false;
+            this.convertedSecondNum.Location = new System.Drawing.Point(290, 165);
+            this.convertedSecondNum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.convertedSecondNum.Name = "convertedSecondNum";
+            this.convertedSecondNum.Size = new System.Drawing.Size(254, 20);
+            this.convertedSecondNum.TabIndex = 3;
+            // 
+            // resultBin
+            // 
+            this.resultBin.Enabled = false;
+            this.resultBin.Location = new System.Drawing.Point(570, 106);
+            this.resultBin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resultBin.Name = "resultBin";
+            this.resultBin.Size = new System.Drawing.Size(337, 20);
+            this.resultBin.TabIndex = 4;
+            // 
+            // resultDeci
+            // 
+            this.resultDeci.Enabled = false;
+            this.resultDeci.Location = new System.Drawing.Point(570, 165);
+            this.resultDeci.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resultDeci.Name = "resultDeci";
+            this.resultDeci.Size = new System.Drawing.Size(337, 20);
+            this.resultDeci.TabIndex = 5;
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(226, 231);
+            this.convertBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(124, 32);
+            this.convertBtn.TabIndex = 6;
+            this.convertBtn.Text = "Calculate";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(274, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(548, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.resultDeci);
+            this.Controls.Add(this.resultBin);
+            this.Controls.Add(this.convertedSecondNum);
+            this.Controls.Add(this.secondNum);
+            this.Controls.Add(this.convertedFirstNum);
+            this.Controls.Add(this.firstNum);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNum;
+        private System.Windows.Forms.TextBox convertedFirstNum;
+        private System.Windows.Forms.TextBox secondNum;
+        private System.Windows.Forms.TextBox convertedSecondNum;
+        private System.Windows.Forms.TextBox resultBin;
+        private System.Windows.Forms.TextBox resultDeci;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Projekt_Architektura/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Projekt_Architektura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void firstNum_TextChanged(object sender, EventArgs e)
+        {
+            ValidateInput();
+
+            if (firstNum.Text.Length == 32) convertedFirstNum.Text = Conversion(firstNum.Text).ToString();
+        }
+
+        private void secondNum_TextChanged(object sender, EventArgs e)
+        {
+            ValidateInput();
+
+            if (secondNum.Text.Length == 32) convertedSecondNum.Text = Conversion(secondNum.Text).ToString();
+        }
+
+        private void firstNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != '\b') e.Handled = true;
+        }
+        private void secondNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != '\b') e.Handled = true;
+        }
+
+        private void ValidateInput()
+        {
+            convertBtn.Enabled = firstNum.Text.Length == 32 && secondNum.Text.Length == 32;
+        }
+
+        private string Multiply(string multiplicand, string multiplier)
+        {
+            int count = multiplicand.Length;
+            
+            string A = "";
+            char Qmin1 = '0';
+            string M = multiplicand;
+
+            char[] minCharM = M.ToCharArray();
+            for (int i = 0; i < count; i++)
+            {
+                A += '0';
+                minCharM[i] = minCharM[i] == '1' ? '0' : '1';
+            }
+            string minM = new string(minCharM);
+            minM = Add(minM, "1");
+
+            string Q = multiplier;
+
+            do
+            {
+                if (Q[Q.Length - 1] == '1' && Qmin1 == '0')
+                {
+                    A = Add(A, minM);
+                }
+                else if (Q[Q.Length - 1] == '0' && Qmin1 == '1')
+                {
+                    A = Add(A, M);
+                }
+
+                Qmin1 = Q[Q.Length - 1];
+
+                Q = A[A.Length - 1] + Q;
+                Q = Q.Substring(0, Q.Length - 1);
+
+                A = A[0] + A;
+                A = A.Substring(0, A.Length - 1);
+
+                count--;
+            } while (count > 0);
+
+            return A + Q;
+        }
+
+        private string Add(string a, string b)
+        {
+            int maxLength = Math.Max(a.Length, b.Length);
+            string result = "";
+            int carry = 0;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int bitA = i < a.Length ? a[a.Length - 1 - i] - '0' : 0;
+                int bitB = i < b.Length ? b[b.Length - 1 - i] - '0' : 0;
+
+                int sum = bitA + bitB + carry;
+                result = (sum % 2) + result;
+                carry = sum / 2;
+            }
+
+            return result;
+        }
+
+        private bool Comparison(string a)
+        {
+            string binTwo = "010";
+
+            if (a.Length > binTwo.Length)
+            {
+                for (int i = 0; i < a.Length - binTwo.Length; i++) binTwo = binTwo[0] + binTwo;
+            }
+            else if (binTwo.Length > a.Length)
+            {
+                for (int i = 0; i < binTwo.Length - a.Length; i++) a = a[0] + a;
+            }
+
+            if (string.CompareOrdinal(a, binTwo) >= 0) return true;
+            
+            return false;
+        }
+
+        private double Conversion(string bin)
+        {
+            int sign = bin[0] == '0' ? 1 : -1;
+
+            int exp = Convert.ToInt32(bin.Substring(1, 8), 2);
+
+            double mantissa = 1.0;
+            string m = bin.Substring(9);
+
+            for (int i = 0; i < m.Length; i++)
+                if (m[i] == '1')
+                    mantissa += Math.Pow(2, -(i + 1));
+
+            return sign * mantissa * Math.Pow(2, exp - 127);
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            const string bias = "10000001";
+
+            string num1 = firstNum.Text;
+            string num2 = secondNum.Text;
+
+            char signChar = num1[0] == num2[0] ? '0' : '1';
+
+            string exponent1 = num1.Substring(1, 8);
+            string exponent2 = num2.Substring(1, 8);
+
+            string mantissa1 = "01" + num1.Substring(9);
+            string mantissa2 = "01" + num2.Substring(9);
+
+            string exponent = Add(Add(exponent1, exponent2), bias);
+
+            string multipliedMantissa = Multiply(mantissa1, mantissa2);
+
+            string integerPart = multipliedMantissa.Substring(0, 4);
+            string mantissa = multipliedMantissa.Substring(multipliedMantissa.Length - 46, 23);
+
+            while (Comparison(integerPart))
+            {
+                mantissa = integerPart[integerPart.Length - 1] + mantissa;
+                mantissa = mantissa.Substring(0, 23);
+
+                integerPart = integerPart.Substring(0, integerPart.Length - 1);
+
+                exponent = Add(exponent, "1");
+            }
+
+            resultBin.Text = signChar + exponent + mantissa;
+            resultDeci.Text = Conversion(resultBin.Text).ToString();
+        }
+    }
+}
